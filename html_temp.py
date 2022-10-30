5b316dfe682a24a9e83142fa34b0594011ddb7fd
zhxy-CN <admin@owo.cab>
2022-10-31 06:59:45 +0800

    Update html_temp.py

--- html_temp.py
+++ html_temp.py
@@ -1,9 +1,29 @@
 CHALLENGE_HTML = """
 <!DOCTYPE html>
-<html>
-  <body>
-    <button type="button" id="login">点击开始验证</button>
-  </body>
+<html lang="zh" class="h-full w-full">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Page</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        body {{
+            color: #495057;
+            background-color: #f5f5f5;
+        }}
+    </style>
+</head>
+<body class="h-full w-full">
+<div class="container mx-auto h-full w-full">
+    <div class="flex flex-col h-full w-full">
+        <div class="m-auto w-full max-w-xl">
+                <div class="rounded-t-lg bg-white ring-0 ring-black ring-opacity-20 m-auto text-6xl
+                submit m-4 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 ">
+                    <button class="w-full p-4 2" id="submit">点击开始验证</button>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
   <script src="./gt.js"></script>
   <script>
     window.initGeetest(
@@ -16,15 +36,15 @@ CHALLENGE_HTML = """
         https: true,
       }},
       (captcha) => {{
-        captcha.appendTo("login");
+        captcha.appendTo("submit");
         captcha.onSuccess(() => {{
-          document.body.innerHTML = "验证成功，页面跳转中...";
+          document.getElementById("submit").innerHTML = "验证成功，页面跳转中...";
           window.location.href = "https://t.me/{2}?start=challenge_" + captcha.getValidate().geetest_validate;
         }});
         captcha.onError(function (error) {{
-          document.body.innerHTML = "发生错误：" + error.msg;
+          document.getElementById("submit").innerHTML = "发生错误：" + error.msg;
         }});
-        document.getElementById("login").onclick = () => {{
+        document.getElementById("submit").onclick = () => {{
           return captcha.verify();
         }};
       }}
