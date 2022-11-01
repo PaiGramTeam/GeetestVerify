b36500709af2875468bc09b1d8e0a2bbb3b5ed00
kotoriのねこ <minamiktr@outlook.com>
2022-11-01 08:28:42 -0500

    Refine verification ui and send use jinja2 template
    - remove `html_temp.py`
    - add `jinja2` dependency
    - add `public/` for web resources

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Request
 from fastapi.responses import HTMLResponse
-from html_temp import CHALLENGE_HTML
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/img", StaticFiles(directory="public/img"), name="img")
+app.mount("/js", StaticFiles(directory="public/js"), name="js")
+
+templates = Jinja2Templates(directory="public/templates")
+
 
 @app.get('/', response_class=HTMLResponse)
-async def challenge_page(
-    *,
-    username: str = Query(..., title="username"),
-    gt: str = Query(..., title="gt"),
-    challenge: str = Query(..., title="challenge")
+async def debug_challenge_page(
+        request: Request,
+        username: str = Query(..., title="username"),
+        uid: str = Query(..., title="uid"),
+        gt: str = Query(..., title="gt"),
+        challenge: str = Query(..., title="challenge")
 ):
-    return CHALLENGE_HTML.format(gt, challenge, username)
-
-
-@app.get("/gt.js", response_class=HTMLResponse)
-async def gt():
-    with open("gt.js", "r", encoding="utf-8") as f:
-        return f.read()
+    user = {
+        "username": username,
+        "uid": uid,
+    }
+    geetest = {
+        "gt": gt,
+        "challenge": challenge,
+    }
+    return templates.TemplateResponse(
+        "example.html",
+        {"request": request, "user": user, "geetest": geetest}
+    )
