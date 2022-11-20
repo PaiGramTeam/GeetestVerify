6fefc23d3d4ef903511eb9e6f99d4fdb1e5ed2f2
洛水居室 <luoshuijs@outlook.com>
2022-11-20 14:53:36 +0800

    🎨 Add webapp

--- main.py
+++ main.py
@@ -11,25 +11,39 @@ app.mount("/js", StaticFiles(directory="public/js"), name="js")
 templates = Jinja2Templates(directory="public/templates")
 
 
-@app.get('/', response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 async def debug_challenge_page(
         request: Request,
         username: str = Query(..., title="username"),
         command: str = Query(..., title="command"),
         uid: str = Query(..., title="uid"),
         gt: str = Query(..., title="gt"),
-        challenge: str = Query(..., title="challenge")
+        challenge: str = Query(..., title="challenge"),
 ):
-    user = {
-        "username": username,
-        "uid": uid,
-        "command": command
-    }
+    user = {"username": username, "uid": uid, "command": command}
     geetest = {
         "gt": gt,
         "challenge": challenge,
     }
     return templates.TemplateResponse(
-        "example.html",
-        {"request": request, "user": user, "geetest": geetest}
+        "example.html", {"request": request, "user": user, "geetest": geetest}
+    )
+
+
+@app.get("/webapp", response_class=HTMLResponse)
+async def debug_challenge_page(
+        request: Request,
+        username: str = Query(..., title="username"),
+        command: str = Query(..., title="command"),
+        uid: str = Query(..., title="uid"),
+        gt: str = Query(..., title="gt"),
+        challenge: str = Query(..., title="challenge"),
+):
+    user = {"username": username, "uid": uid, "command": command}
+    geetest = {
+        "gt": gt,
+        "challenge": challenge,
+    }
+    return templates.TemplateResponse(
+        "webapp.html", {"request": request, "user": user, "geetest": geetest}
     )
